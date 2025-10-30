e0ef380c6cb71264604e75df4d9561ed500d388c
KAMIYO <dev@kamiyo.ai>
2025-10-30 10:46:41 +0100

    Reduce View Documentation link top padding to 0.15rem

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home() {
                                         }}
                                     />
                                 </div>
-                                <div className="pt-1 md:pl-16">
+                                <div className="pt-[0.15rem] md:pl-16">
                                     <LinkButton
                                         href="/api-docs"
                                         title="View API documentation for MCP and x402"
